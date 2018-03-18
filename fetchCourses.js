428a89532d40effd38abd00808b7d9930fe61dbe
Sanjeet Suhag <suhagsanjeet@gmail.com>
2018-03-18 04:46:27 -0400

    Adds GSync

--- fetchCourses.js
+++ fetchCourses.js
@@ -1,24 +1,74 @@
-// Get the schedule table.
-var table = document.getElementsByClassName('datadisplaytable')[0];
-// Get all items in the table.
-var schedule_items = table.querySelectorAll('a');
+// Extract schedule table UI element from DOM.
+var table = document.getElementsByClassName('datadisplaytable')[1];
+// Extract course rows from table.
+var course_rows = table.querySelectorAll('tr');
+// Process UI elements.
+var course_array = Array.from(course_rows).slice(1, -1).filter(validateCourseRow).map(processCourseRow);
 
-var calendar_entries = Array.from(schedule_items).map(item => {
-  // Extract information from HTML <a> items.
-  const info_items = item.innerText.split("\n");
-  const name = info_items[0];
-  const CRN = info_items[1];
-  const time = info_items[2];
-  const location = info_items[3];
-  // Store in JS objects.
-  var calendar_entry = {};
-  calendar_entry["name"] = name;
-  calendar_entry["CRN"] = CRN;
-  calendar_entry["time"] = time;
-  calendar_entry["location"] = location;
-  return calendar_entry;
-});
+// Send data to extension.
+chrome.extension.sendRequest(course_array);
 
+/*
+ * Validates course as non-online.
+ */
+function validateCourseRow(row) {
+  // Get course columns.
+  const course_info = row.querySelectorAll('td');
+  // Make sure it's not an online course.
+  return course_info[8].innerText.match("^[a-zA-Z\(\)]+$");
+}
 
-// Send standard data object to main script.
-chrome.extension.sendRequest(calendar_entries);
+/*
+ * Converts course UI element to standard data object format.
+ */
+function processCourseRow(row) {
+  // Get course columns.
+  const course_info = row.querySelectorAll('td');
+  // Process course info.
+  var course_obj = {};
+  course_obj["CRN"] = course_info[0].innerText;
+  course_obj["code"] = course_info[1].innerText;
+  course_obj["name"] = course_info[2].innerText;
+  course_obj["start_date"] = course_info[6].innerText;
+  course_obj["end_date"] = course_info[7].innerText;
+  course_obj["days"] = course_info[8].innerText;
+  course_obj["start_time"] = getStartTime(course_info[9].innerText);
+  course_obj["end_time"] = getEndTime(course_info[9].innerText);
+  course_obj["location"] = course_info[10].innerText;
+  course_obj["instructor"] = course_info[11].innerText;
+
+  console.log(course_obj);
+
+  return course_obj;
+}
+
+/*
+ * Fetches start time from time string.
+ */
+function getStartTime(time_string) {
+  var start_time = time_string.split(" - ")[0];
+  return convertTo24Hour(start_time);
+}
+
+/*
+ * Fetches end time from time string.
+ */
+function getEndTime(time_string) {
+  var end_time = time_string.split(" - ")[1];
+  return convertTo24Hour(end_time);
+}
+
+/*
+ * Converts 12 HR format to 24 HR format.
+ */
+function convertTo24Hour(time) {
+  var hours = parseInt(time.substr(0, 2));
+  if(time.indexOf('am') != -1 && hours == 12) {
+    time = time.replace('12', '0');
+  }
+  if(time.indexOf('pm')  != -1 && hours < 12) {
+    time = time.replace(hours, (hours + 12));
+  }
+  time.replace(' ', '');
+  return time.replace(/(am|pm)/, '');
+}
--- hello.js
+++ hello.js
@@ -1,9 +1,36 @@
-var allCourses = [];
+var allEvents = [];
 
+/*
+ * Converts date to RFC3339 format.
+ */
+function rfc3339(d) {
+    function pad(n) {
+        return n < 10 ? "0" + n : n;
+    }
+    function timezoneOffset(offset) {
+        var sign;
+        if (offset === 0) {
+            return "Z";
+        }
+        sign = (offset > 0) ? "-" : "+";
+        offset = Math.abs(offset);
+        return sign + pad(Math.floor(offset / 60)) + ":" + pad(offset % 60);
+    }
+    return d.getFullYear() + "-" +
+        pad(d.getMonth() + 1) + "-" +
+        pad(d.getDate()) + "T" +
+        pad(d.getHours()) + ":" +
+        pad(d.getMinutes()) + ":" +
+        pad(d.getSeconds()) +
+        timezoneOffset(d.getTimezoneOffset());
+}
 
+/*
+ * Updates UI with courses.
+ */
 function loadCourses() {
   var courseTable = document.getElementById('courses');
-  for (var i = 0; i < allCourses.length; ++i) {
+  for (var i = 0; i < allEvents.length; ++i) {
     var row = document.createElement('tr');
     var col0 = document.createElement('td');
     var checkbox = document.createElement('input');
@@ -12,17 +39,126 @@ function loadCourses() {
     checkbox.id = 'checked_course_' + i;
     col0.appendChild(checkbox);
     var col1 = document.createElement('td');
-    col1.innerText = allCourses[i].name;
+    col1.innerText = allEvents[i].summary;
     row.appendChild(col0);
     row.appendChild(col1);
     courseTable.appendChild(row);
   }
 }
 
-chrome.extension.onRequest.addListener(function (calendar_entries) {
-  allCourses = calendar_entries;
+/*
+ * Converts course to Google Calendar event.
+ */
+function convertCourseToEvents(course) {
+  var event = {};
+  event["summary"] = course["code"];
+  event["location"] = course["location"];
+  event["description"] = course["name"] + "\n" + "Instructor: " + course["instructor"];
+  event["start"] = { "dateTime" : rfc3339(convertTimeFormat(course["start_date"], course["start_time"])), "timeZone": "America/New_York" };
+  event["originalStartTime"] = { "dateTime" : rfc3339(convertTimeFormat(course["start_date"], course["start_time"])), "timeZone": "America/New_York" };
+  event["end"] = { "dateTime" : rfc3339(convertTimeFormat(course["start_date"], course["end_time"])), "timeZone": "America/New_York" };
+  event["recurrence"] = [ recurrenceString(course["days"], course["end_date"]) ];
+  return event;
+}
+
+/*
+ * Replaces character in string.
+ */
+function setCharAt(str,index,chr) {
+    if(index > str.length - 1) return str;
+    return str.substr(0,index) + chr + str.substr(index+1);
+}
+
+/*
+ * Creates RRULE string for event.
+ */
+function recurrenceString(days, end_date) {
+  var str = "RRULE:";
+  var freq_str = "FREQ=WEEKLY;"
+  var days_str = "BYDAY=";
+  for (var i = 0; i < days.length; i++) {
+    days_str += convertDayCharToDayCode(days.charAt(i)) + ",";
+  }
+  days_str = setCharAt(days_str, days_str.length - 1, ";");
+  var interval_str = "INTERVAL=1;"
+  var until_str = "UNTIL=20180428";
+  //return str + freq_str + interval_str + days_str + until_str;
+  return "RRULE:FREQ=WEEKLY;UNTIL=20180428T035959Z;BYDAY=MO,WE,FR";
+}
+
+//"RRULE:FREQ=WEEKLY;UNTIL=20180428T035959Z;BYDAY=MO,WE,FR"
+
+/*
+ * Converts date and time to Google Calendar format.
+ */
+function convertTimeFormat(date, time) {
+  var date = new Date(date);
+  date.setHours(time.split(":")[0]);
+  date.setMinutes(time.split(":")[1]);
+  //console.log(date);
+  return date;
+}
+
+/*
+ * Converts day character to RRULE day format.
+ */
+function convertDayCharToDayCode(day) {
+  switch (day) {
+    case 'M':
+      return 'MO';
+    case 'T':
+      return 'TU';
+    case 'W':
+      return 'WE';
+    case 'R':
+      return 'TH';
+    case 'F':
+      return 'FR';
+  }
+}
+
+
+
+chrome.extension.onRequest.addListener(function (course_array) {
+  for (var i = 0; i < course_array.length; i++) {
+    allEvents[i] = convertCourseToEvents(course_array[i]);
+  }
   loadCourses();
+  synchronize();
 })
+
+
+/*
+ * Authenticates with Google Calendar and synchronize events.
+ */
+function synchronize() {
+  // Use jQuery to load GAPI script.
+  $.getScript("https://apis.google.com/js/client.js", function() {
+    // Load GAPI.Client.
+    gapi.load('client', function() {
+      console.log('GAPI.Client loaded.');
+      // Load GAPI.Client.Calendar.
+      gapi.client.load('calendar', 'v3', function() {
+        console.log('GAPI.Client.Calendar loaded.');
+        // Authenticate with Chrome identity.
+        chrome.identity.getAuthToken({'interactive': true}, function(token) {
+          // Set GAPI OAuth2 token.
+          gapi.client.setToken({access_token: token});
+
+          // TODO: Loop through all events.
+          var request = gapi.client.calendar.events.insert({
+            'calendarId': 'primary',
+            'resource': allEvents[0]
+          });
+
+          request.execute(function(event) {
+            console.log('Event created: ' + event.htmlLink);
+          });
+        });
+      });
+    });
+  });
+};
 
 window.onload = function() {
   chrome.windows.getCurrent(function (currentWindow) {
